5730cea4fe50d17a4755c93fe70255e7e44987a1
Denis Bystruev <dbystruev@me.com>
2018-09-20 22:17:57 +0300

    Added plane on detected image

--- AR Image Finder/ViewController.swift
+++ AR Image Finder/ViewController.swift
@@ -65,11 +65,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 */
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let imageAnchor = anchor as? ARImageAnchor else {
-            return
-        }
         
-        print(#function, "Нашли картинку \(imageAnchor.referenceImage.name)")
+        switch anchor {
+        case let imageAncor as ARImageAnchor:
+            nodeAdded(node, for: imageAncor)
+        case let planeAncor as ARPlaneAnchor:
+            nodeAdded(node, for: planeAncor)
+        default:
+            print("Нашли якорь, но это не плоскость и не картинка")
+        }
+    }
+    
+    func nodeAdded(_ node: SCNNode, for imageAncor: ARImageAnchor) {
+        let referenceImage = imageAncor.referenceImage
+        
+        let plane = SCNPlane(
+            width: referenceImage.physicalSize.width,
+            height: referenceImage.physicalSize.height
+        )
+        
+        plane.firstMaterial?.diffuse.contents = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+        
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.opacity = 0.25
+        
+        node.addChildNode(planeNode)
+    }
+    
+    func nodeAdded(_ node: SCNNode, for planAncor: ARPlaneAnchor) {
+        
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
